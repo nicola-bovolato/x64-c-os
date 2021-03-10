68b770cceeac156c2528e9d587c1319bbe2e2c24
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-03-10 18:03:14 +0100

    updated kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,5 +1,8 @@
 #include "tty.h"
 #include "printf.h"
+#include "multiboot2.h"
+
+#include <stddef.h>
 
 #if defined(__linux__)
 #error "Not using a cross compiler"
@@ -10,8 +13,16 @@
 #endif
 
 
-void kernel_main(){
+void kernel_main(uint32_t* multiboot_header){
     clear_screen();
     set_color(VGA_WHITE, VGA_GREEN);
-    printf("OK!");
+    printf("OK!\n");
+
+    set_color(VGA_WHITE, VGA_BLACK);
+
+    set_multiboot_info_address(multiboot_header);
+    print_multiboot_info_kernel_memory_region();
+    print_multiboot_info_multiboot_memory_region();
+    print_multiboot_info_mmap_available();
+    print_multiboot_info_elf_sections_used();
 }
