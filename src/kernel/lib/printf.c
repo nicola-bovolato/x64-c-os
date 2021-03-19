c4b73e4664a3227a846be84b8de232ea3b2e27d2
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-03-19 17:24:48 +0100

    fixed printf not printing the right values

--- src/kernel/lib/printf.c
+++ src/kernel/lib/printf.c
@@ -9,15 +9,16 @@
 //size of a pointer + 2 bytes for hex representation + 1 byte for string termination
 #define NUMERIC_BUFFER_SIZE sizeof(void) + 2 + 1
 
-static inline void print_ulong_hex(unsigned long num, char* buf);
-static inline void print_uint_hex (unsigned int  num, char* buf);
-static inline void print_uint     (unsigned int  num, char* buf);
-static inline void print_int      (int num,           char* buf);
+static inline void print_ulong_hex(unsigned long num);
+static inline void print_uint_hex (unsigned int  num);
+static inline void print_uint     (unsigned int  num);
+static inline void print_int      (int num);
+
+static char num_buf[NUMERIC_BUFFER_SIZE];
 
 void printf(char *str, ...) {
 
     va_list args;
-    char num_buf[NUMERIC_BUFFER_SIZE];
 
     va_start(args, str);
 
@@ -27,35 +28,29 @@ void printf(char *str, ...) {
 
             switch(*++str) {
                 case 'i':
-                case 'd': {
-                    volatile int num = va_arg(args, int);
-                    print_int(num, num_buf);
-                } break;
+                case 'd':
+                    print_int(va_arg(args, int));
+                    break;
 
-                case 'u': {
-                    volatile unsigned int num = va_arg(args, unsigned int);
-                    print_uint(num, num_buf);
-                } break;
+                case 'u':
+                    print_uint(va_arg(args, unsigned int));
+                    break;
 
-                case 'x': {
-                    volatile unsigned int num = va_arg(args, unsigned int);
-                    print_uint_hex(num, num_buf);
-                } break;
+                case 'x':
+                    print_uint_hex(va_arg(args, unsigned int));
+                    break;
 
-                case 'p': {
-                    volatile unsigned long num = va_arg(args, unsigned long);
-                    print_ulong_hex(num, num_buf);
-                } break;
+                case 'p':
+                    print_ulong_hex(va_arg(args, void*));
+                    break;
 
-                case 'c': {
-                    volatile unsigned char c = va_arg(args, int);
-                    print_char(c);
-                } break;
+                case 'c':
+                    print_char(va_arg(args, int));
+                    break;
 
-                case 's': {
-                    char* s = va_arg(args, char*);
-                    print(s);
-                } break;
+                case 's':
+                    print(va_arg(args, char*));
+                    break;
 
                 case '%':
                     print_char('%');
@@ -82,24 +77,24 @@ void printf(char *str, ...) {
 }
 
 
-static inline void print_int (int num, char* buf) {
-    itoa(num, buf, 10);
-    print(buf);
+static inline void print_int (int num) {
+    itoa(num, num_buf, 10);
+    print(num_buf);
 }
 
-static inline void print_uint (unsigned int num, char* buf) {
-    utoa(num, buf, 10);
-    print(buf);
+static inline void print_uint (unsigned int num) {
+    utoa(num, num_buf, 10);
+    print(num_buf);
 }
 
-static inline void print_uint_hex (unsigned int num, char* buf) {
-    utoa(num, buf, 16);
-    print(buf);
+static inline void print_uint_hex (unsigned int num) {
+    utoa(num, num_buf, 16);
+    print(num_buf);
 }
 
-static inline void print_ulong_hex (unsigned long num, char* buf) {
-    buf[0] = '0';
-    buf[1] = 'x';
-    ultoa(num, buf + 2, 16);
-    print(buf);
+static inline void print_ulong_hex (unsigned long num) {
+    num_buf[0] = '0';
+    num_buf[1] = 'x';
+    ultoa(num, num_buf + 2, 16);
+    print(num_buf);
 }
